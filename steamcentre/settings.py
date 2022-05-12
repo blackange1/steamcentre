04597bb919ad76ad0a94090f6d531a6da91bf17a
blackange1 <gomerbabenko@gmail.com>
2022-05-12 19:31:44 +0300

    add postgres

--- steamcentre/settings.py
+++ steamcentre/settings.py
@@ -77,11 +77,16 @@ WSGI_APPLICATION = 'steamcentre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbsteam',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
