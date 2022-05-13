bf500709d9fbdea062ccb799e96f0e21586d2c92
blackange1 <gomerbabenko@gmail.com>
2022-05-13 20:51:41 +0300

    sqlite

--- steamcentre/settings.py
+++ steamcentre/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'steamcentre.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbsteam',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'dbsteam',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '1234',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
