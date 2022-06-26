8a30ddcccf6c60c419696a06c6491816089bdcb7
blackange1 <gomerbabenko@gmail.com>
2022-06-26 23:21:36 +0300

    debag footer

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,47 +1,73 @@
-//render menu
-// document.getElementById('mainMenu').innerHTML = menu
+// const sleep = (ms) => {
+//     ms += new Date().getTime();
+//     while (new Date() < ms){}
+// }
 
-//render footer
-// document.getElementById('mainFooter').innerHTML = footer
-
-//render scrollUp
-document.getElementById('body').innerHTML += `
-    <a href="#mainMenu" id="scroll__up" class="scroll__up" uk-scroll>
-        <div  uk-icon="icon: chevron-up; ratio: 1.5" ></div>
-    </a>
-    `
-
-let scrollUp = document.getElementById("scroll__up")
-let isShow = false
-setInterval(() => {
-    if (window.scrollY > 1500) {
-        if (!isShow) {
-            scrollUp.classList.add("show-scroll_up")
-            isShow = true
-        }
-    } else {
-        if (isShow) {
-            scrollUp.classList.remove("show-scroll_up")
-            isShow = false
-        }
+document.addEventListener('DOMContentLoaded', function(){
+    const print = (obj) => {
+        console.log(obj)
     }
-}, 1000);
+//    //render menu
+//    // document.getElementById('mainMenu').innerHTML = menu
+//
+//    //render footer
+//    // document.getElementById('mainFooter').innerHTML = footer
+//
+    //render scrollUp
+    // sleep(500)
+    let elementBody = document.getElementById('body');
+    let a = document.createElement('a');
+    a.id = 'scroll__up'
+    a.href = '#body'
+    a.classList = 'scroll__up'
+    a.setAttribute('uk-scroll', '')
 
+    let div = document.createElement('div')
+    div.setAttribute('uk-icon', 'icon: chevron-up; ratio: 1.5')
+    
+    a.appendChild(div)
+    elementBody.appendChild(a)
 
-// render footerYear
-function renderYear() {
-    let footerEnd = document.getElementById('year');
-    let date = new Date();
-    footerEnd.innerText = '2020 - ' + date.getFullYear().toString();
-}
-
-renderYear()
-
-//menu
-let subMenuHeadline = document.getElementsByClassName('sub_menu_headline')
-for (let el of subMenuHeadline) {
-    el.onclick = function () {
-        el.nextElementSibling.classList.toggle('visible')
-    }
-}
-
+    print(a)
+    print(typeof(a))
+//    document.getElementById('body').innerHTML += `
+//    <a href="#mainMenu" id="scroll__up" class="scroll__up" uk-scroll>
+//        <div  uk-icon="icon: chevron-up; ratio: 1.5" ></div>
+//    </a>
+//    `
+//
+   let scrollUp = document.getElementById("scroll__up")
+   let isShow = false
+   setInterval(() => {
+   if (window.scrollY > 1500) {
+       if (!isShow) {
+           scrollUp.classList.add("show-scroll_up")
+           isShow = true
+       }
+   } else {
+       if (isShow) {
+           scrollUp.classList.remove("show-scroll_up")
+           isShow = false
+       }
+   }
+   }, 1000);
+//
+//
+//    // render footerYear
+   function renderYear() {
+   let footerEnd = document.getElementById('year');
+   let date = new Date();
+   footerEnd.innerText = '2020 - ' + date.getFullYear().toString();
+   }
+//
+   renderYear()
+//
+//    //menu
+//    let subMenuHeadline = document.getElementsByClassName('sub_menu_headline')
+//    for (let el of subMenuHeadline) {
+//    el.onclick = function () {
+//        el.nextElementSibling.classList.toggle('visible')
+//    }
+//    }
+// alert(document.getElementById('body'))
+}, false);
